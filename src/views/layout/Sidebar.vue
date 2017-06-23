e6062fe525d7dd9964ecd2832ae1c2ad2482b6fb
wentao978 <476788978@qq.com>
2017-06-23 14:36:51 +0800

    add a simple demo

---
+++ src/views/layout/Sidebar.vue
@@ -0,0 +1,26 @@
+<template>
+    <el-menu mode="vertical" theme="dark" :unique-opened=true :default-active="$route.path" >
+        <sidebar-item :routes='permission_routers'></sidebar-item>
+    </el-menu>
+</template>
+
+<script>
+    import { mapGetters } from 'vuex';
+    import SidebarItem from './SidebarItem';
+    export default {
+      components: { SidebarItem },
+      computed: {
+        ...mapGetters([
+          'permission_routers'
+        ])
+      },
+      created: function () {
+        //   debugger;
+      }
+    }
+</script>
+<style rel="stylesheet/scss" lang="scss" scoped>
+    .el-menu {
+        min-height: 100%;
+    }
+</style>
