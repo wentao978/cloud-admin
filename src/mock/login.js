78482e73d87813fa126c9e6c3e99fcba047c7157
wentao978 <476788978@qq.com>
2017-06-27 10:25:16 +0800

    add some

--- src/mock/login.js
+++ src/mock/login.js
@@ -5,7 +5,7 @@ const userMap = {
     role: ['admin'],
     token: 'admin',
     introduction: '我是超级管理员',
-    avatar: 'http://q3.qlogo.cn/g?b=qq&k=OUyBPXfzMcJo3Ud6Mb6gRQ&s=140&t=1483322487',
+    avatar: 'https://q3.qlogo.cn/g?b=qq&k=OUyBPXfzMcJo3Ud6Mb6gRQ&s=140&t=1483322487',
     name: '超级管理员',
     uid: '001'
   },
@@ -13,7 +13,7 @@ const userMap = {
     role: ['editor'],
     token: 'editor',
     introduction: '我是编辑',
-    avatar: 'http://q3.qlogo.cn/g?b=qq&k=OUyBPXfzMcJo3Ud6Mb6gRQ&s=140&t=1483322487',
+    avatar: 'https://q3.qlogo.cn/g?b=qq&k=OUyBPXfzMcJo3Ud6Mb6gRQ&s=140&t=1483322487',
     name: '普通编辑',
     uid: '002'
 
@@ -22,7 +22,7 @@ const userMap = {
     role: ['develop'],
     token: 'develop',
     introduction: '我是开发',
-    avatar: 'http://q3.qlogo.cn/g?b=qq&k=OUyBPXfzMcJo3Ud6Mb6gRQ&s=140&t=1483322487',
+    avatar: 'https://q3.qlogo.cn/g?b=qq&k=OUyBPXfzMcJo3Ud6Mb6gRQ&s=140&t=1483322487',
     name: '工程师',
     uid: '003'
   }
